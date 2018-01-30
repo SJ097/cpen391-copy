e6fd60d3a6a5888e5dca5db437632c80e5bf1abd
Matthew Chernoff <chernoff.matthew@gmail.com>
2018-01-29 19:20:19 -0800

    Exercise 1.4 and updates to Readme

---
+++ exercise1.4/software/Exercise141/obj/default/Touchscreen.d
@@ -0,0 +1,6 @@
+obj/default/Touchscreen.o: Touchscreen.c ../Exercise141_bsp//HAL/inc/io.h \
+ ../Exercise141_bsp//HAL/inc/alt_types.h
+
+../Exercise141_bsp//HAL/inc/io.h:
+
+../Exercise141_bsp//HAL/inc/alt_types.h:
