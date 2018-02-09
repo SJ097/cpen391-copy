8507a71d206ead52ea2549b1c75066121dfb1652
Matthew Chernoff <chernoff.matthew@gmail.com>
2018-02-08 16:18:14 -0800

    Exercise1.8 software

---
+++ exercise1.8/software/Exercise181/Wifi.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <io.h>
+
+#define Wifi_Control (volatile unsigned char *)	(0x4000240)
+#define Wifi_Status (volatile unsigned char *)	(0x4000240)
+#define Wifi_TxData (volatile unsigned char *)	(0x4000242)
+#define Wifi_RxData (volatile unsigned char *)	(0x4000242)
+#define Wifi_Baud 	(volatile unsigned char *)	(0x4000244)
+
+char getcharWifi(void);
+int putcharWifi(int);
+
+void Init_Wifi(void){
+
+	IOWR_8DIRECT(Wifi_Baud, 0, 1);
+	IOWR_8DIRECT(Wifi_Control, 0, 149);
+
+	//IOWR_8DIRECT(Wifi_RxData, 0, 0);
+	int i;
+	for(i = 0; i < 10; i++){
+		//Send a few newline (\n) chars to synchronize baud
+		putcharWifi(0x0a);
+	}
+}
+
+void sendString(char* str){
+	int i;
+	for(i = 0; i < strlen(str); i++){
+		putcharWifi(str[i]);
+	}
+}
+
+int putcharWifi(int c){
+	int status;
+
+	while(1) {
+		status = IORD_8DIRECT(Wifi_Status, 0);
+		if((status & 2) == 2)
+			break;
+	}
+
+	IOWR_8DIRECT(Wifi_TxData, 0, c);
+	return c;
+}
+
+char getcharWifi(void){
+		int status;
+
+		while(1) {
+			status = IORD_8DIRECT(Wifi_Status,0);
+
+			if( (status & 1) == 1)
+				break;
+		}
+
+		return (char) IORD_8DIRECT(Wifi_RxData,0);
+}
---
+++ exercise1.8/software/Exercise181/obj/default/Wifi.d
@@ -0,0 +1,6 @@
+obj/default/Wifi.o: Wifi.c ../Exercise181_bsp//HAL/inc/io.h \
+ ../Exercise181_bsp//HAL/inc/alt_types.h
+
+../Exercise181_bsp//HAL/inc/io.h:
+
+../Exercise181_bsp//HAL/inc/alt_types.h:
